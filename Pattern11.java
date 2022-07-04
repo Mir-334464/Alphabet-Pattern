359925473c74cdcb0f5188f44202daf31009de11
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-07-04 16:58:55 +0530

    Add files via upload

---
+++ Pattern11.java
@@ -0,0 +1,21 @@
+class Pattern11
+{
+
+   public static void main(String[]args)
+
+      {
+ 
+         int i,j;
+
+        for(i=65; i<=70; i++)
+
+         {
+
+           for(j=i; j>=65; j--)
+
+            System.out.print((char)j+" ");
+        
+         System.out.println();
+      }
+   }
+}
---
+++ Pattern9.java
@@ -0,0 +1,21 @@
+class Pattern9
+{
+
+   public static void main(String[]args)
+
+      {
+
+         int i,j;
+
+         for(i=70; i>=65; i--)
+          {
+
+            for(j=i; j>=65; j--)
+
+             System.out.print((char)j+" ");
+           
+           System.out.println();
+        }
+     }
+}
+          
